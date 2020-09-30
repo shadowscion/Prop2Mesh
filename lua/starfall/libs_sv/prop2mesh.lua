b8bcce7a4b580b0f43da8f1f74b5e636be606032
shadowscion <shadowscion@users.noreply.github.com>
2020-09-30 12:32:17 -0500

    Update prop2mesh.lua

--- lua/starfall/libs_sv/prop2mesh.lua
+++ lua/starfall/libs_sv/prop2mesh.lua
@@ -1,4 +1,3 @@
--- -----------------------------------------------------------------------------
 local checkluatype = SF.CheckLuaType
 local registerprivilege = SF.Permissions.registerPrivilege
 
@@ -37,165 +36,181 @@ local function P2M_AntiSpam(controller, action)
 	return true
 end
 
+--- Library for creating and manipulating p2m controllers".
+-- @name prop2mesh
+-- @class library
+-- @libtbl p2m_library
+SF.RegisterLibrary("prop2mesh")
 
--- -----------------------------------------------------------------------------
-SF.RegisterLibrary("p2m")
+--- prop2mesh type
+-- @name p2m
+-- @class type
+-- @libtbl p2m_methods
 SF.RegisterType("p2m", true, false, nil, "Entity")
 
 return function(instance)
-local checkpermission = instance.player ~= SF.Superuser and SF.Permissions.check or function() end
+	local checkpermission = instance.player ~= SF.Superuser and SF.Permissions.check or function() end
 
+	local p2m_library = instance.Libraries.prop2mesh
+	local p2m_methods, p2m_meta, wrap, unwrap = instance.Types.p2m.Methods, instance.Types.p2m, instance.Types.p2m.Wrap, instance.Types.p2m.Unwrap
+	local ents_methods, ent_meta, ewrap, eunwrap = instance.Types.Entity.Methods, instance.Types.Entity, instance.Types.Entity.Wrap, instance.Types.Entity.Unwrap
+	local ang_meta, awrap, aunwrap = instance.Types.Angle, instance.Types.Angle.Wrap, instance.Types.Angle.Unwrap
+	local vec_meta, vwrap, vunwrap = instance.Types.Vector, instance.Types.Vector.Wrap, instance.Types.Vector.Unwrap
+	local mtx_meta, mwrap, munwrap = instance.Types.VMatrix, instance.Types.VMatrix.Wrap, instance.Types.VMatrix.Unwrap
 
--- -----------------------------------------------------------------------------
-local p2m_library = instance.Libraries.p2m
-local p2m_methods, p2m_meta, wrap, unwrap = instance.Types.p2m.Methods, instance.Types.p2m, instance.Types.p2m.Wrap, instance.Types.p2m.Unwrap
-local ents_methods, ent_meta, ewrap, eunwrap = instance.Types.Entity.Methods, instance.Types.Entity, instance.Types.Entity.Wrap, instance.Types.Entity.Unwrap
-local ang_meta, awrap, aunwrap = instance.Types.Angle, instance.Types.Angle.Wrap, instance.Types.Angle.Unwrap
-local vec_meta, vwrap, vunwrap = instance.Types.Vector, instance.Types.Vector.Wrap, instance.Types.Vector.Unwrap
-local mtx_meta, mwrap, munwrap = instance.Types.VMatrix, instance.Types.VMatrix.Wrap, instance.Types.VMatrix.Unwrap
+	local getent
+	instance:AddHook("initialize", function()
+		instance.data.p2ms = {p2ms = {}}
+		getent = instance.Types.Entity.GetEntity
+		p2m_meta.__tostring = ent_meta.__tostring
+	end)
 
-local getent
-instance:AddHook("initialize", function()
-	instance.data.p2ms = {p2ms = {}}
-	getent = instance.Types.Entity.GetEntity
-	p2m_meta.__tostring = ent_meta.__tostring
-end)
+	instance:AddHook("deinitialize", function()
+		local p2ms = instance.data.p2ms.p2ms
+		for p2m, _ in pairs(p2ms) do
+			if p2m:IsValid() then
+				p2m:RemoveCallOnRemove("starfall_p2m_delete")
+				p2mOnDestroy(p2m, p2ms, instance.player)
+				p2m:Remove()
+			end
+		end
+	end)
 
-instance:AddHook("deinitialize", function()
-	local p2ms = instance.data.p2ms.p2ms
-	for p2m, _ in pairs(p2ms) do
-		if p2m:IsValid() then
-			p2m:RemoveCallOnRemove("starfall_p2m_delete")
-			p2mOnDestroy(p2m, p2ms, instance.player)
-			p2m:Remove()
+	local function getp2m(self)
+		local ent = unwrap(self)
+		if ent:IsValid() then
+			return ent
+		else
+			SF.Throw("Entity is not valid.", 3)
 		end
 	end
-end)
 
-local function getp2m(self)
-	local ent = unwrap(self)
-	if ent:IsValid() then
-		return ent
-	else
-		SF.Throw("Entity is not valid.", 3)
-	end
-end
+	--- Creates a p2m controller.
+	-- @param pos The position to create the p2m controller
+	-- @param ang The angle to create the p2m controller
+	-- @param uvscale (Optional) The uvscale to give the p2m controller
+	-- @param meshscale (Optional) The meshscale to give the p2m controller
+	-- @return The p2m object
+	function p2m_library.create(pos, ang, uvscale, meshscale)
+		local pos = vunwrap(pos)
+		local ang = aunwrap(ang)
 
+		local ply = instance.player
+		local p2mdata = instance.data.p2ms.p2ms
 
--- -----------------------------------------------------------------------------
-function p2m_library.create(pos, ang, uvscale, meshscale)
-	local pos = vunwrap(pos)
-	local ang = aunwrap(ang)
+		local p2ment = ents.Create(ent_class)
+		if p2ment and p2ment:IsValid() then
+			p2ment:SetPlayer(ply)
+			p2ment:SetPos(SF.clampPos(pos))
+			p2ment:SetAngles(ang)
+			p2ment:SetModel("models/hunter/plates/plate.mdl")
+			p2ment:SetMaterial("p2m/grid")
+			p2ment:CallOnRemove("starfall_p2m_delete", p2mOnDestroy, p2mdata, ply)
+			p2ment:Spawn()
 
-	local ply = instance.player
-	local p2mdata = instance.data.p2ms.p2ms
+			p2ment:SetSolid(SOLID_NONE)
+			p2ment:SetMoveType(MOVETYPE_NONE)
+			p2ment:DrawShadow(false)
+			p2ment:Activate()
 
-	local p2ment = ents.Create(ent_class)
-	if p2ment and p2ment:IsValid() then
-		p2ment:SetPlayer(ply)
-		p2ment:SetPos(SF.clampPos(pos))
-		p2ment:SetAngles(ang)
-		p2ment:SetModel("models/hunter/plates/plate.mdl")
-		p2ment:SetMaterial("p2m/grid")
-		p2ment:CallOnRemove("starfall_p2m_delete", p2mOnDestroy, p2mdata, ply)
-		p2ment:Spawn()
+			p2ment.isSFP2M = true
+			p2ment.DoNotDuplicate = true
+			p2ment.SFP2MResevoir = {}
 
-		p2ment:SetSolid(SOLID_NONE)
-		p2ment:SetMoveType(MOVETYPE_NONE)
-		p2ment:DrawShadow(false)
-		p2ment:Activate()
-
-		p2ment.isSFP2M = true
-		p2ment.DoNotDuplicate = true
-		p2ment.SFP2MResevoir = {}
-
-		if uvscale ~= nil then
-			checkluatype(uvscale, TYPE_NUMBER)
-			controller:SetNWInt("P2M_TSCALE", math.Clamp(uvscale, 0, 512))
-		end
-		if meshscale ~= nil then
-			checkluatype(meshscale, TYPE_NUMBER)
-			controller:SetNWFloat("P2M_MSCALE", math.Clamp(meshscale, 0.1, 1))
-		end
-
-		if ply ~= SF.Superuser then gamemode.Call("PlayerSpawnedSENT", ply, p2ment) end
-
-		p2mdata[p2ment] = true
-
-		return wrap(p2ment)
-	end
-end
-
-function p2m_methods:pushModel(model, pos, ang, scale, invert, flat, clips)
-	local p2ment = getp2m(self)
-	if #p2ment.SFP2MResevoir > LIMIT_MODELS then
-		SF.Throw("You have reached the model hardcap on this p2m controller", 3)
-		return
-	end
-
-	checkluatype(model, TYPE_STRING)
-
-	local entry  = {
-		mdl = SF.NormalizePath(model),
-		pos = vunwrap(pos),
-		ang = aunwrap(ang),
-	}
-
-	if scale ~= nil then
-		entry.scale = vunwrap(scale)
-	end
-
-	if invert ~= nil then
-		checkluatype(invert, TYPE_BOOL)
-		entry.inv = invert
-	end
-
-	if flat ~= nil then
-		checkluatype(flat, TYPE_BOOL)
-		entry.flat = flat
-	end
-
-	if clips ~= nil then
-		checkluatype(clips, TYPE_TABLE)
-
-		if #clips == 0 or #clips % 2 ~= 0 then
-			SF.Throw("Clips table must contain an even number of elements", 3)
-		end
-
-		local array = {}
-
-		for i = 1, #clips, 2 do
-			if i > LIMIT_CLIPS*2 then
-				break
+			if uvscale ~= nil then
+				checkluatype(uvscale, TYPE_NUMBER)
+				p2ment:SetNWInt("P2M_TSCALE", math.Clamp(uvscale, 0, 512))
+			end
+			if meshscale ~= nil then
+				checkluatype(meshscale, TYPE_NUMBER)
+				p2ment:SetNWFloat("P2M_MSCALE", math.Clamp(meshscale, 0.1, 1))
 			end
 
-			local origin = vunwrap(clips[i + 0])
-			local normal = vunwrap(clips[i + 1])
-			normal:Normalize()
+			if ply ~= SF.Superuser then gamemode.Call("PlayerSpawnedSENT", ply, p2ment) end
 
-			array[#array + 1] = { n = normal, d = origin:Dot(normal) }
+			p2mdata[p2ment] = true
 
-			::invalid::
-		end
-
-		if #array > 0 then
-			entry.clips = array
+			return wrap(p2ment)
 		end
 	end
 
-	p2ment.SFP2MResevoir[#p2ment.SFP2MResevoir + 1] = entry
-end
+	--- Adds a model to the build stack.
+	-- @param model model to add
+	-- @param pos local pos offset
+	-- @param ang local ang offset
+	-- @param scale (optional) model scale
+	-- @param render_inside (optional)
+	-- @param render_flat (optional) use flat normal shading
+	-- @param clips (optional) table of alternating clip origins and clip normals
+	function p2m_methods:pushModel(model, pos, ang, scale, render_inside, render_flat, clips)
+		local p2ment = getp2m(self)
+		if #p2ment.SFP2MResevoir > LIMIT_MODELS then
+			SF.Throw("You have reached the model hardcap on this p2m controller", 3)
+			return
+		end
 
-function p2m_methods:build()
-	local p2ment = getp2m(self)
-	if not P2M_AntiSpam(p2ment, "build") then
-		SF.Throw("Don't spam", 3)
+		checkluatype(model, TYPE_STRING)
+
+		local entry  = {
+			mdl = SF.NormalizePath(model),
+			pos = vunwrap(pos),
+			ang = aunwrap(ang),
+		}
+
+		if scale ~= nil then
+			entry.scale = vunwrap(scale)
+		end
+
+		if render_inside ~= nil then
+			checkluatype(render_inside, TYPE_BOOL)
+			entry.inv = render_inside
+		end
+
+		if render_flat ~= nil then
+			checkluatype(render_flat, TYPE_BOOL)
+			entry.render_flat = render_flat
+		end
+
+		if clips ~= nil then
+			checkluatype(clips, TYPE_TABLE)
+
+			if #clips == 0 or #clips % 2 ~= 0 then
+				SF.Throw("Clips table must contain an even number of elements", 3)
+			end
+
+			local array = {}
+
+			for i = 1, #clips, 2 do
+				if i > LIMIT_CLIPS*2 then
+					break
+				end
+
+				local origin = vunwrap(clips[i + 0])
+				local normal = vunwrap(clips[i + 1])
+				normal:Normalize()
+
+				array[#array + 1] = { n = normal, d = origin:Dot(normal) }
+
+				::invalid::
+			end
+
+			if #array > 0 then
+				entry.clips = array
+			end
+		end
+
+		p2ment.SFP2MResevoir[#p2ment.SFP2MResevoir + 1] = entry
 	end
-	if #p2ment.SFP2MResevoir > 0 then
-		p2ment:SetModelsFromTable(p2ment.SFP2MResevoir)
-		p2ment.SFP2MResevoir= {}
+
+	--- Build the model stack.
+	function p2m_methods:build()
+		local p2ment = getp2m(self)
+		if not P2M_AntiSpam(p2ment, "build") then
+			SF.Throw("Don't spam", 3)
+		end
+		if #p2ment.SFP2MResevoir > 0 then
+			p2ment:SetModelsFromTable(p2ment.SFP2MResevoir)
+			p2ment.SFP2MResevoir= {}
+		end
 	end
-end
-
-
 end
